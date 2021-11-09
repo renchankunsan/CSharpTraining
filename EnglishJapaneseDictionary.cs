cfb7e802a18b4be3e57f93b1132fd05e0fa04af0
南雲 蓮 <58325382+renchankunsan@users.noreply.github.com>
2021-11-09 18:52:53 +0900

    Add files via upload

---
+++ EnglishJapaneseDictionary.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Training.WorkBook.EnglishJapaneseDictionary
+{
+    public class EnglishJapaneseDictionary
+    {
+        /// <summary>
+        /// 英和辞書を作ろう
+        /// CreateEnglishJapaneseDicメソッド内で複数の英和辞書が作られています。
+        /// 引数として辞書の種類と日本語訳したい英単語が渡されるので、適切な辞書を選んで日本語訳を出力して下さい。
+        /// 表示内容は下記の通り。
+        ///     ・辞書の種類が存在しないとき
+        ///         →「xxという辞書は存在しません。」
+        ///         例：「bikeという辞書は存在しません。」
+        ///     ・辞書は存在するが翻訳対象の英単語が存在しないとき
+        ///         →「xxの辞書にxxは存在しません。」
+        ///         例：「animalの辞書にmonkeyは存在しません。」
+        ///     ・辞書と翻訳対象が見つかったとき
+        ///         →「xxの日本語名はxxです。」
+        ///         例：「animalの日本語名は犬です。」
+        /// </summary>
+        public void ShowJapanese(string dicType, string translationTarget)
+        {
+            Dictionary<string, Dictionary<string, string>> EJDictionary = CreateEnglishJapaneseDic();
+
+            // ここから下にコードを書く
+            if (EJDictionary.ContainsKey(dicType))
+            {
+                if (EJDictionary[dicType].ContainsKey(translationTarget))
+                {
+                    Console.WriteLine("{0}の日本語名は{1}です。", translationTarget, EJDictionary[dicType][translationTarget]);
+                }
+                else
+                {
+                    Console.WriteLine("{0}の辞書に{1}は存在しません。", dicType, translationTarget);
+                }
+            }
+            else
+            {
+                Console.WriteLine("{0}という辞書は存在しません。", dicType);
+            }
+        }
+
+        /// <summary>
+        /// 英和辞書作成
+        /// animalというkeyに対して動物の英和辞書
+        /// fruitというkeyに対して果物の英和辞書
+        /// faceというkeyに対して顔のパーツの英和辞書
+        /// ※変更不要
+        /// </summary>
+        /// <returns></returns>
+        private static Dictionary<string, Dictionary<string, string>> CreateEnglishJapaneseDic()
+        {
+            // 動物の英和辞書を作ろう
+            Dictionary<string, string> animalDic = new Dictionary<string, string>();
+            animalDic["dog"] = "犬";
+            animalDic["cat"] = "猫";
+            animalDic["bird"] = "鳥";
+
+            // 果物の英和辞書を作ろう
+            Dictionary<string, string> fruitDic = new Dictionary<string, string>();
+            fruitDic["apple"] = "りんご";
+            fruitDic["grape"] = "ぶどう";
+            fruitDic["peach"] = "もも";
+
+            // 顔のパーツの英和辞書を作ろう
+            Dictionary<string, string> faceDic = new Dictionary<string, string>();
+            faceDic["nose"] = "鼻";
+            faceDic["ear"] = "耳";
+            faceDic["eye"] = "目";
+
+            // keyに辞書名、valueに英和辞書を取るようなDictionaryの宣言
+            Dictionary<string, Dictionary<string, string>> EJDictionary = new Dictionary<string, Dictionary<string, string>>();
+            EJDictionary["animal"] = animalDic;
+            EJDictionary["fruit"] = fruitDic;
+            EJDictionary["face"] = faceDic;
+
+            return EJDictionary;
+        }
+
+
+
+    }
+}
---
+++ HundredSquareCalc.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Training.WorkBook.HundredSquareCalc
+{
+    public class HundredSquareCalc
+    {
+        /// <summary>
+        /// 百ます計算
+		/// 【ルール】
+		/// ・縦軸を10個、横軸を10個とする。
+		/// ・縦の数字と横の数字の積を出力していく。
+		/// ・<<二次元のリスト>>を用いること。
+		/// ・縦軸も横軸も1-10を順番に用いる。
+        /// 
+		/// 【出力結果例】
+		/// 1,2,3,4,5,6,7,8,9,10
+		/// 2,4,6,8,10,12,14,16,18,20
+		/// 3,6,9,12,15,18,21,24,27,30
+		/// 4,8,12,16,20,24,28,32,36,40
+		/// 5,10,15,20,25,30,35,40,45,50
+		/// 6,12,18,24,30,36,42,48,54,60
+		/// 7,14,21,28,35,42,49,56,63,70
+		/// 8,16,24,32,40,48,56,64,72,80
+		/// 9,18,27,36,45,54,63,72,81,90
+		/// 10,20,30,40,50,60,70,80,90,100
+        /// </summary>
+        public void ShowHundredSquareCalc()
+        {
+			List<int> numList1 = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+			List<int> numList2 = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+			List<List<int>> numLists = new List<List<int>>()
+			{
+				numList1,numList2
+			};
+
+			//	微妙な気がする
+			for (int i1 = 0; i1 < numLists[0].Count; i1++)
+            {
+				for (int i2 = 0; i2 < numLists[1].Count; i2++)
+                {
+					Console.Write(numLists[0][i1] * numLists[1][i2]);
+
+					if (i2 < numLists[1].Count - 1)
+                    {
+						Console.Write(", ");
+                    }
+                }
+
+				Console.WriteLine();
+            }
+
+		}
+    }
+}
